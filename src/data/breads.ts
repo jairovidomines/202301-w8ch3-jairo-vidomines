c653d0cdf07dbc2af0844737225651d9ed157eae
Jairo Vidomines <jvidomines@gmail.com>
2023-03-01 21:24:18 +0100

    Test loadBreads reducer

---
+++ src/data/breads.ts
@@ -0,0 +1,32 @@
+import { Breads } from "./types";
+
+export const breads: Breads = [
+  {
+    id: 1,
+    productName: "Hogaza",
+    price: 3,
+    image: "string",
+    isAvailable: true,
+  },
+  {
+    id: 2,
+    productName: "Barra",
+    price: 1.5,
+    image: "string",
+    isAvailable: true,
+  },
+  {
+    id: 3,
+    productName: "Pan de molde",
+    price: 3.5,
+    image: "string",
+    isAvailable: true,
+  },
+  {
+    id: 4,
+    productName: "Cuadrado",
+    price: 2,
+    image: "string",
+    isAvailable: true,
+  },
+];
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,7 +1,8 @@
 import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
+import { breadsReducer } from "./features/breads/breadsSlice";
 
 export const store = configureStore({
-  reducer: {},
+  reducer: { breads: breadsReducer },
 });
 
 export type AppDispatch = typeof store.dispatch;
